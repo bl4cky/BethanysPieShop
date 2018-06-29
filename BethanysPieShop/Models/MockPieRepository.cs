cc5d62e865d9a78672dc28e7d4a539945e7240fb
Stefan Schwartz <bl4cky1989@gmail.com>
2018-06-29 23:38:55 +0200

    start model

---
+++ BethanysPieShop/Models/MockPieRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class MockPieRepository : IPieRepository
+    {
+        private List<Pie> _pies;
+        public MockPieRepository()
+        {
+            if(_pies == null)
+            {
+                InitializePies();
+            }
+        }
+
+        private void InitializePies()
+        {
+            _pies = new List<Pie>()
+            {
+                new Pie{}
+            }
+        }
+
+        public IEnumerable<Pie> GetAllPies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pie GetPieById(int pieId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
